b18525641f479230f250cdeb267e2c40ea415209
kochatkov <vlad.kochatkov@gmail.com>
2020-06-23 21:00:45 +0300

    Updates

---
+++ precache-manifest.fbcd197671356298e0eb419323f26dd1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6f17b0d21c980e4dd10fbc7eeb7befa",
+    "url": "/logivations/index.html"
+  },
+  {
+    "revision": "610c85c97224b857a520",
+    "url": "/logivations/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7bac2072420e3c87b99c",
+    "url": "/logivations/static/js/2.c3b22ffe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/logivations/static/js/2.c3b22ffe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "610c85c97224b857a520",
+    "url": "/logivations/static/js/main.b796b306.chunk.js"
+  },
+  {
+    "revision": "d1dbec87bf9f1c260ad0",
+    "url": "/logivations/static/js/runtime-main.46e505a0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/logivations/static/media/logo.5d5d9eef.svg"
+  }
+]);
